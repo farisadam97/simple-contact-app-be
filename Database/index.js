77fc9e9830cab1a945afe95d19097596a2fef3eb
Faris Adam <farisadam97@gmail.com>
2023-10-13 14:45:16 +0700

    minor changes

--- Database/index.js
+++ Database/index.js
@@ -1,11 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const db = new sqlite3.Database(path.resolve("contact.db"), (err) => {
+const db = new sqlite3.Database(path.resolve("contact.sqlite"), (err) => {
   if (err) {
     console.log("Error opening database :" + err.message);
   } else {
     db.run(
-      "CREATE TABLE employees( \
+      "CREATE TABLE contact( \
                contact_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
                full_name NVARCHAR(40)  NOT NULL,\
                phone_number NVARCHAR(16),\
